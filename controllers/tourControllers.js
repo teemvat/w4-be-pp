e35ba107a05a12c40529197b0145692460e29fe3
teemvat <teemu.vataja@metropolia.fi>
2024-11-11 14:10:43 +0200

    step 2

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -8,7 +8,7 @@ const createTour = (req, res) => {
   const { name, info, image, price } = req.body;
   const newTour = Tour.addOne(name, info, image, price);
   if (newTour) {
-    res.json(newTour);
+    res.status(201).json(newTour); // 201 Created
   } else {
     res.status(500).json({ message: "Fail to create tour" });
   }
@@ -39,7 +39,7 @@ const deleteTour = (req, res) => {
   const tourId = req.params.tourId;
   const isDeleted = Tour.deleteOneById(tourId);
   if (isDeleted) {
-    res.json({ message: "Deleted successfully" });
+    res.status(204).send(); // 204 No Content
   } else {
     res.status(404).json({ message: "Tour not found" });
   }
